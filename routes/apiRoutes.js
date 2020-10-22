47eeacc86ffbd792c7849458d96b58129570682a
Matt Neal <impulseimg@gmail.com>
2020-10-22 11:55:19 -0400

    API routes added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,4 +10,20 @@ router.get("/api/workouts", function (req, res) {
         .catch(err => res.json(err))
 });
 
+router.get("/api/workouts/range", function (req, res) {
+    workout.find({}).limit(30)
+});
+
+router.post("/api/workouts", function (req, res) {
+    console.log(res);
+});
+
+router.put("/api/workouts/:id", function (req, res) {
+    console.log(req.body)
+    workout.findByIdAndUpdate(req.params.id, {
+        $push: { exercises: req.body }
+    }
+});
+
+
 module.exports = router
